badca45e0122ccee85a321a5bfab3b388d45cf20
mgariev <thepun599@gmail.com>
2017-09-01 12:24:31 +0300

    some renaming

--- concurrency/src/main/java/org/thepun/concurrency/queue/Demultiplexer.java
+++
@@ -1,9 +0,0 @@
-package org.thepun.concurrency.queue;
-
-public interface Demultiplexer<T> extends QueueTail<T> {
-
-    QueueHead<T> createConsumer();
-
-    void destroyConsumer(QueueHead<T> consumer);
-
-}
