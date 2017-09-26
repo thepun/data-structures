b7fb314393d52a97827d907e7bab22a02281d030
mgariev <thepun599@gmail.com>
2017-09-26 20:49:44 +0300

    atomic buffer work in progress

--- src/main/java/org/thepun/data/queue/AtomicBufferRouter.java
+++ src/main/java/org/thepun/data/queue/AtomicBufferRouter.java
@@ -3,7 +3,6 @@ package org.thepun.data.queue;
 import java.util.Arrays;
 
 import org.thepun.unsafe.ArrayMemory;
-import org.thepun.unsafe.MemoryFence;
 
 public final class AtomicBufferRouter<T> implements Router<T> {
 
@@ -13,8 +12,8 @@ public final class AtomicBufferRouter<T> implements Router<T> {
 
     private final int size;
     private final int mask;
-    private final long[] index;
     private final Object[] data;
+    private final Generation[] generations;
 
     private AtomicBufferConsumer<T>[] consumers;
     private AtomicBufferProducer<T>[] producers;
@@ -30,12 +29,16 @@ public final class AtomicBufferRouter<T> implements Router<T> {
         size = (int) Math.pow(2, pow);
         mask = size - 1;
         data = new Object[size];
-        index = new long[size * 8];
         consumers = new AtomicBufferConsumer[0];
         producers = new AtomicBufferProducer[0];
 
-        for (int i = 0; i < size; i++) {
-            ArrayMemory.setLong(index, i * 8, i - size + 4);
+        generations = new Generation[10];
+        for (int i = 0; i < 10; i++) {
+            generations[i] = new Generation(i);
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            data[i] = generations[1];
         }
     }
 
@@ -138,6 +141,10 @@ public final class AtomicBufferRouter<T> implements Router<T> {
     private void updateConsumers(AtomicBufferConsumer<T>[] newConsumers) {
         consumers = newConsumers;
 
+        for (int i = 0; i < producers.length; i++) {
+            producers[i].consumers = newConsumers;
+        }
+
         /*int count = newConsumers.length;
         int step = size / count;
         for (int i = 0; i < count; i++) {
@@ -152,9 +159,14 @@ public final class AtomicBufferRouter<T> implements Router<T> {
 
         private final int size;
         private final int mask;
-        private final long[] index;
         private final Object[] data;
+        private final Generation[] generations;
 
+        private int generationIndex;
+        private Generation currentGeneration;
+        private Generation previousGeneration;
+        private AtomicBufferConsumer<T>[] consumers;
+        private AlignedLong producerReadCounter;
         private AlignedLong producerWriteCounter;
 
         private AtomicBufferProducer(AtomicBufferRouter<T> parent) {
@@ -163,37 +175,61 @@ public final class AtomicBufferRouter<T> implements Router<T> {
             size = parent.size;
             mask = parent.mask;
             data = parent.data;
-            index = parent.index;
+            consumers = parent.consumers;
+            generations = parent.generations;
+
+            producerReadCounter = new AlignedLong();
+            producerReadCounter.set(0);
 
             producerWriteCounter = new AlignedLong();
             producerWriteCounter.set(0);
+
+            generationIndex = 1;
+            currentGeneration = generations[1];
+            previousGeneration = generations[0];
         }
 
         @Override
         public boolean addToTail(T element) {
+            long localReadCounter = producerReadCounter.get();
             long localWriteCounter = producerWriteCounter.get();
 
-            int i = (int) (localWriteCounter & mask);
-            long k;
-            for (;;) {
-                k = ArrayMemory.getLong(index, i * 8);
-                if (k < localWriteCounter - size + 3) {
-                    return false;
-                } else if (k == localWriteCounter - size + 3) {
-                    continue;
-                } else if (k == localWriteCounter - size + 4) {
-                    if (ArrayMemory.compareAndSwapLong(index, i * 8, k, localWriteCounter + 1)) {
-                        ArrayMemory.setObject(data, i, element);
-                        MemoryFence.store();
-                        ArrayMemory.setLong(index, i * 8, localWriteCounter + 2);
+            if (localWriteCounter >= localReadCounter + size) {
+                localReadCounter = consumers[0].consumerReadCounter.get();
+                for (int i = 1; i < consumers.length; i++) {
+                    long localReadCounterFromConsumer = consumers[i].consumerReadCounter.get();
+                    if (localReadCounter > localReadCounterFromConsumer) {
+                        localReadCounter = localReadCounterFromConsumer;
+                    }
+                }
+                producerReadCounter.set(localReadCounter);
+            }
+
+            int index;
+            Object currentElement;
+            while (localWriteCounter < localReadCounter + size) {
+                if (localWriteCounter % size == 0) {
+                    previousGeneration = currentGeneration;
+                    currentGeneration = generations[++generationIndex % 10];
+                }
+
+                index = (int) (localWriteCounter & mask);
+
+                currentElement = data[index];
+                if (currentElement == previousGeneration) {
+                    if (ArrayMemory.compareAndSwapObject(data, index, previousGeneration, element)) {
                         producerWriteCounter.set(localWriteCounter + 1);
                         return true;
                     }
+                    localWriteCounter++;
+                } else if (currentElement == currentGeneration) {
+                    localWriteCounter++;
+                } else {
+                    return false;
                 }
-
-                localWriteCounter += 1;
-                i = (int) (localWriteCounter & mask);
             }
+
+            return false;
         }
     }
 
@@ -204,9 +240,12 @@ public final class AtomicBufferRouter<T> implements Router<T> {
 
         private final int size;
         private final int mask;
-        private final long[] index;
         private final Object[] data;
+        private final Generation[] generations;
 
+        private int generationIndex;
+        private Generation currentGeneration;
+        private Generation previousGeneration;
         private AlignedLong consumerReadCounter;
 
         private AtomicBufferConsumer(AtomicBufferRouter<T> parent) {
@@ -215,36 +254,43 @@ public final class AtomicBufferRouter<T> implements Router<T> {
             size = parent.size;
             mask = parent.mask;
             data = parent.data;
-            index = parent.index;
+            generations = parent.generations;
 
             consumerReadCounter = new AlignedLong();
             consumerReadCounter.set(0);
+
+            generationIndex = 1;
+            currentGeneration = generations[1];
+            previousGeneration = generations[0];
         }
 
         @Override
         public T removeFromHead() {
             long localReadCounter = consumerReadCounter.get();
 
-            int i = (int) (localReadCounter & mask);
-            long k;
+            int index;
+            Object element;
             for (;;) {
-                k = ArrayMemory.getLong(index, i * 8);
-                if (k < localReadCounter) {
-                    return null;
-                } else if (k == localReadCounter + 1) {
-                    continue;
-                } else if (k == localReadCounter + 2) {
-                    if (ArrayMemory.compareAndSwapLong(index, i * 8, k, localReadCounter + 3)) {
-                        T element = (T) ArrayMemory.getObject(data, i);
-                        MemoryFence.load();
-                        ArrayMemory.setLong(index, i * 8, localReadCounter + 4);
-                        consumerReadCounter.set(localReadCounter + 1);
-                        return element;
-                    }
+                if (localReadCounter % size == 0) {
+                    previousGeneration = currentGeneration;
+                    currentGeneration = generations[++generationIndex % 10];
                 }
 
-                localReadCounter += 1;
-                i = (int) (localReadCounter & mask);
+                index = (int) (localReadCounter & mask);
+
+                element = data[index];
+                if (element == previousGeneration) {
+                    return null;
+                } else if (element == currentGeneration) {
+                    localReadCounter++;
+                    consumerReadCounter.set(localReadCounter);
+                } else if (ArrayMemory.compareAndSwapObject(data, index, element, currentGeneration)) {
+                    consumerReadCounter.set(localReadCounter + 1);
+                    return (T) element;
+                } else {
+                    localReadCounter++;
+                    consumerReadCounter.set(localReadCounter);
+                }
             }
         }
     }
---
+++ src/main/java/org/thepun/data/queue/Generation.java
@@ -0,0 +1,14 @@
+package org.thepun.data.queue;
+
+final class Generation {
+
+    private final int value;
+
+    Generation(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
