19afe80331262240cc3018d6dbacc973e2a10541
marat <mgariev@spotware.com>
2017-10-04 18:45:58 +0300

    hashtable work in progress

--- src/main/java/org/thepun/data/match/LongToLongHashtable.java
+++ src/main/java/org/thepun/data/match/LongToLongHashtable.java
@@ -5,7 +5,38 @@ package org.thepun.data.match;
  */
 public class LongToLongHashtable {
 
+    public static final long ELEMENT_NOT_FOUND = Long.MAX_VALUE;
+
+
+    private int size;
+    private long[] data;
+
+    public LongToLongHashtable() {
+        size = 10;
+        data = new long[20];
+    }
+
     public long get(long key) {
+        int localSize = size;
+        long[] localData = data;
+        int hash = (int) (key % localSize);
+
+
+
         return 0;
     }
+
+    public void set(long key, long value) {
+        if (value == ELEMENT_NOT_FOUND) {
+            throw new IllegalStateException("Value " + value + " not allowed");
+        }
+
+        int localSize = size;
+        long[] localData = data;
+        int hash = (int) (key % localSize);
+
+
+    }
+
+
 }
